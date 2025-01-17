e49e05d6f443551b42752092aec6315cc2b1f193
pratiksolkar <2023bit045@sggs.ac.in>
2025-01-17 13:53:34 +0530

    This code is used for User Identification and Authentication ( while login time )

---
+++ security/Authentication.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+char already_SET_USERNAME[] = "Pratik";                                 //pre-defined details
+char already_SET_PASSWORD[] = "Pratik@123";                             //username & password
+
+char username[50];                                                      //initialize space for new
+char password[50];                                                      //username & password
+//#define MAX_USERNAME_LEN 50
+//#define MAX_PASSWORD_LEN 50
+
+int main() {
+    
+    printf("User Identification and Authentication System\n");
+    
+    printf("Enter username: ");
+    scanf("%s", username);
+
+    printf("Enter password: ");
+    scanf("%s", password);
+
+    if (strcmp(username, already_SET_USERNAME) == 0 && strcmp(password, already_SET_PASSWORD) == 0){     //comparing new details 
+       printf("Access Granted! Login Successful");                                                       //with pre-defined details
+    }
+    else{
+        printf("Access Denied! Please try again");
+    }
+}
