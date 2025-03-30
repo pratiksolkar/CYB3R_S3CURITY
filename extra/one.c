11b43c14b49e488c0823b79938b3fed0e24a51c8
pratiksolkar <2023bit045@sggs.ac.in>
2025-03-30 20:14:20 +0530

    c program

---
+++ extra/one.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+  int i=3;
+  switch (i==3){
+    case 1:
+    printf("Hello PRATIKgg\n");
+    break;
+    
+    case 2
+    printf("Hello BHAI");
+    break;
+    
+    default:
+    printf("Okk Bhai");
+  }
+  return 0;
+}
